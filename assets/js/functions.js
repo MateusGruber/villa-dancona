63c149999926a11d9331be81b21876679811b578
mateusgruber <mateus.gruber@madeinweb.com.br>
2020-03-08 21:34:22 -0300

    Fixing js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
             slidesToShow: 1,
             infinite: false,
             prevArrow: '<button type="button" class="slick-prev"></button>',
-            nextArrow: '<button type="button" class="slick-next"></button>'
+            nextArrow: '<button type="button" class="slick-next"></button>',
             responsive: [
                 {
                     breakpoint: 768,
